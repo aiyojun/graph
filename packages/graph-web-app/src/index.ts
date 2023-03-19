50e2279591321d25425db57953568b7dc2ef1aa1
DJun <1608450902@qq.com>
2023-03-19 17:04:46 +0800

    nothing important

--- packages/graph-web-app/src/index.ts
+++ packages/graph-web-app/src/index.ts
@@ -13,73 +13,6 @@ const graph = new Graph()
 
 new VisionApp(graph)
 
-
-
-
-
-// const node1 = graph.createNode({
-//     x: 0, y: 0,
-//     w: 200, h: 120,
-//     title: 'Node1',
-//     inPortNumber: 2,
-//     outPortNumber: 3
-// })
-
-// const node2 = graph.createNode({
-//     x: 300, y: 200,
-//     w: 200, h: undefined,
-//     title: 'Node3',
-//     // inPortNumber: 2,
-//     // outPortNumber: 3
-// }, [
-//     {type: 0, i: 0, x: 0, y: 30, uuid: ''},
-// ])
-
-// const card1 =
-//     new DescriptionCard('Algorithm', 'Convex', 'Do convex calculation with a matrix.')
-//         .mount(node1.el)
-
-// const card2 =
-//     new DescriptionCard('Matrix Balance', 'Histogram', 'Make data more flat.')
-//         .mount(node2.el)
-
-// const card2 =
-//     new CyberPanel().mount(node2.el, {
-//         type: 'VLinearLayout',
-//         children: [
-//             {type: 'label', label: 'Control Panel'},
-//             {type: 'input', label: '次表面半径', value: '10', specific: { placeholder: '次表面半径' }},
-//             {type: 'number', label: '金属度', value: 12, specific: { placeholder: '金属度' }},
-//             {type: 'input', label: 'IOR', value: '43', specific: { placeholder: 'IOR' }},
-//             {
-//                 type: 'HLinearLayout', children: [
-//                     {
-//                         type: 'button', label: 'Simulate', specific: {
-//                             click: () => {
-//                                 const snapshot = graph.snapshot()
-//                                 console.info(JSON.stringify(snapshot))
-//                                 graph
-//                                     .changeWireStyle('dynamic')
-//                             }
-//                         }
-//                     },
-//                     {
-//                         type: 'button', label: 'Stop', specific: {
-//                             click: () => {
-//                                 const snapshot = graph.snapshot()
-//                                 console.info(JSON.stringify(snapshot))
-//                                 graph
-//                                     .changeWireStyle('static')
-//                             }
-//                         }
-//                     },
-//                 ]
-//             }
-//         ]
-//     })
-    // new GridCard()
-    //     .mount(node2.el)
-
 const panelRoot = inject(
     document.body,
     `<div style="box-shadow: 0 0 10px 5px rgba(0,0,0,0.25); position: fixed; top: 0; bottom: 0; right: 0; width: 240px; background: #333;"></div>`
@@ -91,32 +24,5 @@ new CyberPanel().mount(panel, {
     children: [
         {type: 'label', label: 'Control Panel'},
         {type: 'button', label: 'Use chinese', specific: { click: () => {  } }},
-        // {type: 'input', label: '次表面半径', value: '10', specific: { placeholder: '次表面半径' }},
-        // {type: 'number', label: '金属度', value: 12, specific: { placeholder: '金属度' }},
-        // {type: 'input', label: 'IOR', value: '43', specific: { placeholder: 'IOR' }},
-        // {
-        //     type: 'HLinearLayout', children: [
-        //         {
-        //             type: 'button', label: 'Simulate', specific: {
-        //                 click: () => {
-        //                     const snapshot = graph.snapshot()
-        //                     console.info(JSON.stringify(snapshot))
-        //                     graph
-        //                         .changeWireStyle('dynamic')
-        //                 }
-        //             }
-        //         },
-        //         {
-        //             type: 'button', label: 'Stop', specific: {
-        //                 click: () => {
-        //                     const snapshot = graph.snapshot()
-        //                     console.info(JSON.stringify(snapshot))
-        //                     graph
-        //                         .changeWireStyle('static')
-        //                 }
-        //             }
-        //         },
-        //     ]
-        // }
     ]
 })
